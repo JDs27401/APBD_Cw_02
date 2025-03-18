475a8ddd8144a920d295f71fb661cb9d5627db05
JDs27401 <s27401@pjwstk.edu.pl>
2025-03-18 22:52:41 +0100

    1st commit to project, 3rd try

---
+++ MP01/MP01/Container.cs
@@ -0,0 +1,22 @@
+﻿namespace MP01;
+
+public class Container
+{
+    private static Dictionary<string, Container> containers = new Dictionary<string, Container>();
+    private static int mainId = 0;
+    private int id;
+    private int cargoWeight;            //in kilograms
+    private int emptyWeight = 2_000;    //in kilograms
+    private int maxWeight = 24_000;     //in kilograms
+    private int height = 243;           //in centimeters
+    private int depth = 605;            //in centimeters
+    private string serialNumber;
+    private ContainerType type;
+
+    public Container(string serialNumber, ContainerType type)
+    {
+        this.serialNumber = "CON-{type}-{main}";
+        this.type = type;
+        id = mainId++;
+    }
+}
