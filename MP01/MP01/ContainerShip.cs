ba32c7de98d7adee57a3a37a107a1b1c8cef6057
JDs27401 <s27401@pjwstk.edu.pl>
2025-03-29 21:47:28 +0100

    prawie gotowe, tylko dopisać testy

---
+++ MP01/MP01/ContainerShip.cs
@@ -0,0 +1,107 @@
+﻿using System.Data;
+
+namespace MP01;
+
+public class ContainerShip {
+    private Dictionary<String, Container> _containers = new Dictionary<String, Container>();
+
+    private static double _maxSpeed = 16.0; //w węzłach
+    private string _shipName;
+    private double _speed;
+    private double _maxContainersWeight;
+    private int _maxContainerCapacity;
+    private double _containersWeight;
+    private int _containerCapacity;
+    
+    public ContainerShip(string name, double speed, double MaxContainerWeight, int MaxContainerCapacity) {
+        SetShipName(name);
+        SetSpeed(speed);
+        SetMaxContainerWeight(MaxContainerWeight);
+        SetMaxContainerCapacity(MaxContainerCapacity);
+    }
+
+    public void LoadContainer(Container container) {
+        AddContainer(container);
+    }
+
+    public void LoadContainers(IEnumerable<Container> containers) {
+        foreach (Container con in containers) {
+            AddContainer(con);
+        }
+    }
+
+    public void UnloadContainer(String containerName) {
+        if (!_containers.ContainsKey(containerName)) {
+            throw new Exception("Container not found: " + containerName);
+        }
+        RemoveContainer(containerName);
+    }
+
+    public void ChangeShip(ContainerShip second, string containerName) {
+        second.AddContainer(_containers[containerName]);
+        RemoveContainer(containerName);
+    }
+    
+    private void AddContainer(Container container) {
+        if (_containersWeight + (container.GetEmptyWeight() + container.GetCargoWeight())/1000 > _maxContainersWeight) {
+            throw new Exception("Container weight capacity exceeded.");
+        }
+
+        if (_containerCapacity >= _maxContainerCapacity) {
+            throw new Exception("Container capacity exceeded.");
+        }
+        _containersWeight += (container.GetCargoWeight() + container.GetEmptyWeight())/1000;
+        _containerCapacity++;
+        _containers.Add(container.GetSerialNumber(), container);
+    }
+
+    private void RemoveContainer(string containerName) {
+        _containersWeight -= (_containers[containerName].GetCargoWeight() + _containers[containerName].GetEmptyWeight())/1000;
+        _containerCapacity--;
+        _containers.Remove(containerName);
+    }
+    
+    public void SetShipName(string name) {
+        if (name == null) {
+            throw new NoNullAllowedException("New name is a null");
+        }
+
+        if (name.Length == 0) {
+            throw new ArgumentException("Name is an empty string");
+        }
+        _shipName = name;
+    }
+
+    public void SetSpeed(double speed) {
+        if (speed > 16.0) {
+            throw new ArgumentException("New speed is too big");
+        }
+        _speed = speed;
+    }
+
+    public void SetMaxContainerWeight(double maxContainerWeight) {
+        if (maxContainerWeight < 0) {
+            throw new ArgumentException("MaxContainerWeight is negative");
+        }
+        _maxContainersWeight = maxContainerWeight;
+    }
+
+    public void SetMaxContainerCapacity(int maxContainerCapacity) {
+        if (maxContainerCapacity < 0) {
+            throw new ArgumentException("MaxContainerCapacity is negative");
+        }
+        _maxContainerCapacity = maxContainerCapacity;
+    }
+
+    public override string ToString() {
+        return
+            $"{nameof(_shipName)}: {_shipName}, {nameof(_speed)}: {_speed}, {nameof(_maxContainersWeight)}: {_maxContainersWeight}, {nameof(_maxContainerCapacity)}: {_maxContainerCapacity}, {nameof(_containersWeight)}: {_containersWeight}, {nameof(_containerCapacity)}: {_containerCapacity}";
+    }
+
+    public void Show() {
+        Console.WriteLine("Ship info:\n" + this + "\nContainers:");
+        foreach (Container con in _containers.Values) {
+            Console.WriteLine(con);
+        }
+    }
+}
--- MP01/MP01/Program.cs
+++ MP01/MP01/Program.cs
@@ -4,19 +4,24 @@
 
 using MP01;
 
-// Container con1 = new Container(ContainerType.C);
-// Console.WriteLine(con1);
-// con1.loadContainer(22_000);
-// Console.WriteLine("bajo jajo");
-// Console.WriteLine(con1);
 LiquidContainer lCon1 = new LiquidContainer(true);
 lCon1.LoadContainer(10000);
 Console.WriteLine(lCon1);
-lCon1.UnloadContainer();
-Console.WriteLine(lCon1);
+// lCon1.UnloadContainer();
+// Console.WriteLine(lCon1);
 
 GasContainer gCon1 = new GasContainer(3);
 gCon1.LoadContainer(10000);
 Console.WriteLine(gCon1);
-gCon1.UnloadContainer();
-Console.WriteLine(gCon1);
+// gCon1.UnloadContainer();
+// Console.WriteLine(gCon1);
+
+ContainerShip cShip1 = new ContainerShip("Statek 1", 12, 5000, 50);
+Console.WriteLine(cShip1);
+
+cShip1.LoadContainer(lCon1);
+Console.WriteLine(cShip1);
+cShip1.Show();
+
+cShip1.UnloadContainer(lCon1.GetSerialNumber());
+Console.WriteLine(cShip1);
